a0d4613ce963eaa287d5b64e67aba1ab5e36b498
Kariuki11 <irungukenneth433@gmail.com>
2024-06-11 02:47:43 +0300

    New changes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //CALCULATOR PROGRAMME
+
+const display = document.getElementById("display");
+
+function appendToDisplay(){
+    display.value += Input;
+}
+
+function clearDisplay(){
+
+}
+
+function calculate(){
+
+}
