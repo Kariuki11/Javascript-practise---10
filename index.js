2f04748d853c23efc9b2e7840ba1554cbab97b77
Kariuki11 <irungukenneth433@gmail.com>
2024-06-11 02:59:43 +0300

    New changes

--- index.js
+++ index.js
@@ -11,5 +11,10 @@ function clearDisplay(){
 }
 
 function calculate(){
-    display.value = eval(display.value)
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
 }
